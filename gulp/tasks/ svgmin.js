0f2aaa7957532a4bc5462af7caed5c181582198f
Steph <stephaniergreene94@gmail.com>
2019-03-31 15:21:28 +1100

    trial svg

--- gulp/tasks/ svgmin.js
+++ gulp/tasks/ svgmin.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var svgmin = require('gulp-svgmin');
+var gulp = require('gulp'),
+svgmin = require('gulp-svgmin');
 
 gulp.task('svgmin', function () {
-    return gulp.src('../../app/assets/images/icons.1/capitalviz-logo.svg')
+    return gulp.src('./app/assets/images/icons.1/capitalviz-logo.svg')
         .pipe(svgmin())
-        .pipe(gulp.dest('./out'));
+        .pipe(gulp.dest('./app/temp/images'));
 });
