a2343d8a28b54be0971aab8e79ce272c872cabad
Steph <stephaniergreene94@gmail.com>
2019-03-31 16:40:50 +1100

    blank

--- gulp/tasks/ svgmin.js
+++ gulp/tasks/ svgmin.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 svgmin = require('gulp-svgmin');
 
 gulp.task('svgmin', function () {
-    return gulp.src('./app/assets/images/icons.1/capitalviz-logo.svg')
+    return gulp.src('./app/assets/images/icons.1')
         .pipe(svgmin())
         .pipe(gulp.dest('./app/temp/images'));
 });
