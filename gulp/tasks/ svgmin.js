a6066979193ebc8f67814604388a5ffd792acfb9
mctouch <mctouched@gmail.com>
2019-03-31 15:24:36 +1100

    stuff

--- gulp/tasks/ svgmin.js
+++ gulp/tasks/ svgmin.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var svgmin = require('gulp-svgmin');
 
-gulp.task('svgmin', function () {
+gulp.task('svgMin', function () {
     return gulp.src('../../app/assets/images/icons.1/capitalviz-logo.svg')
         .pipe(svgmin())
         .pipe(gulp.dest('./out'));
