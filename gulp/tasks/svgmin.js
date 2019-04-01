2708088b40406274f5ddc9c75f5326237b0d47fe
Steph <stephaniergreene94@gmail.com>
2019-04-01 18:54:27 +1100

    sprite changes

--- gulp/tasks/svgmin.js
+++ gulp/tasks/svgmin.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
 svgmin = require('gulp-svgmin');
 
 gulp.task('svgmin', function () {
-    return gulp.src('./app/assets/images/icons.1')
+    return gulp.src('./app/assets/images/icons.1/*')
         .pipe(svgmin())
-        .pipe(gulp.dest('./app/temp/images'));
+    return gulp.src('./app/assets/images/icons')
+    .pipe(gulp.dest('./app/assets/images/icons'));
 });
