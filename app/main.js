1685bb3ce71297e9155e78eb3ceec6d4b5fa265b
Steph <stephaniergreene94@gmail.com>
2019-04-20 16:03:07 +1000

    JavaScript redirection

--- app/main.js
+++ app/main.js
@@ -1,30 +1,34 @@
-/////////////////////////////////////////////
-/////////////   CONTACT PAGE   ////////////
-
-console.log('hello world');
+// CONTACT CONTROLLER
 
 $(document).ready(function() {
-    $('.submit').click(function(event) {
-    event.preventDefault()     
-    console.log('clicked button'); //don't send form until validated
+    $('.submit'). click(function(event) {
+        event.preventDefault()
+        console.log('clicked button')
 
-    var email = $('.email').val()
-    var message = $('.message').val()
-    var statusElm = $('.status')
-        statusElm.empty();
+        var email, message, firstName, lastName;
 
+        email = $('.form__email').val(),
+        message = $('.form__message').val(),
+        firstName = $('.form__firstName').val(),
+        lastName = $('form__lastName').val()
+        
 
-    if (email.length > 5 && email.includes('@') && email.includes('.')) {
-        statusElm.append('<div>this is vaild</div>')
-    } else {
-        statusElm.append('<div>this is not vaild</div>');
-    }
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            console.log('Email is valid')
+        } else {
+            console.log('Email is not valid')
+        }
 
-    if (message.length >= 10) {
-        console.log('is vaild')
-    } else {
-        console.log('is not vaild')
-    }
+        if(message.length >= 10) {
+        console.log('Message is valid')
+        } else {
+            console.log('Message not is valid')
+        }
 
+        if(firstName.length >= 2) {
+            console.log('First name is valid')
+            } else {
+                console.log('First name not is valid')
+            }
     });
 });
