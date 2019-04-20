3a029cdc1f34c434beca8bf7c76da6778f9e2cb5
Steph <stephaniergreene94@gmail.com>
2019-04-21 00:01:32 +1000

    experimenting with validation

--- app/main.js
+++ app/main.js
@@ -1,34 +1,35 @@
 // CONTACT CONTROLLER
+console.log('It works')
 
 $(document).ready(function() {
     $('.submit'). click(function(event) {
         event.preventDefault()
         console.log('clicked button')
 
-        var email, message, firstName, lastName;
+        var email, message, firstName, statusElm;
 
         email = $('.form__email').val(),
         message = $('.form__message').val(),
         firstName = $('.form__firstName').val(),
-        lastName = $('form__lastName').val()
+        statusElm = $('.status') //Validation process
+        statusElm.empty();
         
 
         if(email.length > 5 && email.includes('@') && email.includes('.')) {
-            console.log('Email is valid')
+            
         } else {
-            console.log('Email is not valid')
+            statusElm.append('<div>*Email is not valid</div>')
         }
 
         if(message.length >= 10) {
-        console.log('Message is valid')
+        
         } else {
-            console.log('Message not is valid')
+            statusElm.append('<div>*Message is not valid</div>')
         }
 
         if(firstName.length >= 2) {
-            console.log('First name is valid')
             } else {
-                console.log('First name not is valid')
+                statusElm.append('<div>*First name is not valid</div>')
             }
     });
 });
